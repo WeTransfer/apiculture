1b272f3e2888b3fe90ddb3dc55d6b94e4b91647f
Dennis van der Vliet <dennis.vandervliet@gmail.com>
2018-05-24 20:02:02 +0200

    Beef up coverage

--- spec/apiculture_spec.rb
+++ spec/apiculture_spec.rb
@@ -2,26 +2,26 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Apiculture" do
   include Rack::Test::Methods
-  
+
   before(:each) { @app_class = nil }
   def app
     @app_class or raise "No @app_class defined in the example"
   end
-  
+
   context 'as API definition DSL' do
     it 'allows all the standard Siantra DSL to go through without modifications' do
       @app_class = Class.new(Apiculture::App) do
         extend Apiculture
-      
+
         post '/things/*' do
           params.inspect
         end
       end
-      
+
       post '/things/a/b/c/d', {'foo' => 'bar'}
       expect(last_response.body).to eq("{\"foo\"=>\"bar\", \"splat\"=>[\"a/b/c/d\"], \"captures\"=>[\"a/b/c/d\"]}")
     end
-    
+
     it 'flags :captures as a reserved Sinatra parameter when used as a URL param' do
       expect {
         Class.new(Apiculture::App) do
@@ -31,7 +31,7 @@ describe "Apiculture" do
         end
       }.to raise_error(/\:captures is a reserved magic parameter name/)
     end
-    
+
     it 'flags :captures as a reserved Sinatra parameter when used as a request param' do
       expect {
         Class.new(Apiculture::App) do
@@ -41,7 +41,7 @@ describe "Apiculture" do
         end
       }.to raise_error(/\:captures is a reserved magic parameter name/)
     end
-    
+
     it 'flags :splat as a reserved Sinatra parameter when used as a URL param' do
       expect {
         Class.new(Apiculture::App) do
@@ -51,7 +51,7 @@ describe "Apiculture" do
         end
       }.to raise_error(/\:splat is a reserved magic parameter name/)
     end
-    
+
     it 'flags :splat as a reserved Sinatra parameter when used as a request param' do
       expect {
         Class.new(Apiculture::App) do
@@ -61,7 +61,7 @@ describe "Apiculture" do
         end
       }.to raise_error(/\:splat is a reserved magic parameter name/)
     end
-    
+
     it 'flags URL and request params of the same name' do
       expect {
         Class.new(Apiculture::App) do
@@ -72,12 +72,12 @@ describe "Apiculture" do
         end
       }.to raise_error(/\:id mentioned twice/)
     end
-    
+
     it "defines a basic API that can be called" do
       $created_thing = nil
       @app_class = Class.new(Apiculture::App) do
         extend Apiculture
-      
+
         desc "Create a Thing with a name"
         route_param :id, "The ID of the thing"
         required_param :name, "Name of the thing", String
@@ -86,53 +86,53 @@ describe "Apiculture" do
           'Wild success'
         end
       end
-    
+
       post '/thing/123', {name: 'Monsieur Thing'}
       expect(last_response.body).to eq('Wild success')
       expect($created_thing).to eq({id: '123', name: 'Monsieur Thing'})
     end
-    
+
     it "serves the API documentation at a given URL using serve_api_documentation_at" do
       $created_thing = nil
       @app_class = Class.new(Apiculture::App) do
         extend Apiculture
-      
+
         desc "Create a Thing with a name"
         required_param :name, "Name of the thing", String
         api_method( :post, '/thing/:id') {}
         serve_api_documentation_at('/documentation')
       end
-    
+
       get '/documentation'
       expect(last_response['Content-Type']).to include('text/html')
       expect(last_response.body).to include('Create a Thing')
     end
-    
+
     it 'raises when a required param is not provided' do
       @app_class = Class.new(Apiculture::App) do
         extend Apiculture
-      
+
         required_param :name, "Name of the thing", String
         api_method :post, '/thing' do
           raise "Should never be called"
         end
       end
-    
+
       expect {
         post '/thing', {}
       }.to raise_error('Missing parameter :name')
     end
-  
+
     it 'verifies the parameter type' do
       @app_class = Class.new(Apiculture::App) do
         extend Apiculture
-      
+
         required_param :number, "Number of the thing", Integer
         api_method :post, '/thing' do
           raise "Should never be called"
         end
       end
-    
+
       expect {
         post '/thing', {number: '123'}
       }.to raise_error('Received String, expected Integer for :number')
@@ -144,10 +144,10 @@ describe "Apiculture" do
           value == "Magic word"
         end
       end.new
-      
+
       @app_class = Class.new(Apiculture::App) do
         extend Apiculture
-      
+
         required_param :pretty_please, "Only a magic word will do", custom_matcher
         api_method :post, '/thing' do
           'Ohai!'
@@ -165,13 +165,13 @@ describe "Apiculture" do
     it 'suppresses parameters that are not defined in the action definition' do
       @app_class = Class.new(Apiculture::App) do
         extend Apiculture
-      
+
         api_method :post, '/thing' do
           raise ":evil_ssh_injection should have wiped from params{}" if params[:evil_ssh_injection]
           'All is well'
         end
       end
-      
+
       post '/thing', {evil_ssh_injection: 'I am Homakov!'}
       expect(last_response).to be_ok
     end
@@ -196,7 +196,7 @@ describe "Apiculture" do
           'All is well'
         end
       end
-      
+
       post '/vanilla-thing/123456'
       expect(last_response).to be_ok
 
@@ -213,11 +213,11 @@ describe "Apiculture" do
           json_response({was_created: true})
         end
       end
-      
+
       post '/api/123'
       expect(last_response.status).to eq(201)
     end
-    
+
     it 'raises when describing a route parameter that is not included in the path' do
       expect {
         Class.new(Apiculture::App) do
@@ -227,11 +227,24 @@ describe "Apiculture" do
         end
       }.to raise_error('Parameter :thing_id not present in path "/thing/:id"')
     end
-    
+
+    it 'returns a 404 when a non existing route is called' do
+      @app_class = Class.new(Apiculture::App) do
+        extend Apiculture
+
+        api_method :post, '/api' do
+          [1]
+        end
+      end
+
+      post '/api-404'
+      expect(last_response.status).to eq(404)
+    end
+
     it 'applies a symbol typecast by calling a method on the parameter value' do
       @app_class = Class.new(Apiculture::App) do
         extend Apiculture
-      
+
         required_param :number, "Number of the thing", Integer, :cast => :to_i
         api_method :post, '/thing' do
           raise "Not cast" unless params[:number] == 123
@@ -245,7 +258,7 @@ describe "Apiculture" do
     it 'ensures current behaviour for route params is not changed' do
       @app_class = Class.new(Apiculture::App) do
         extend Apiculture
-      
+
         route_param :number, "Number of the thing"
         api_method :post, '/thing/:number' do
           raise "Casted to int" if params[:number] == 123
@@ -256,10 +269,22 @@ describe "Apiculture" do
       expect(last_response.body).to eq('Total success')
     end
 
+    it 'supports returning a rack triplet' do
+      @app_class = Class.new(Apiculture::App) do
+        extend Apiculture
+        api_method :get, '/rack' do
+          [402, {'X-Money-In-The-Bank' => 'yes, please'}, ['Buy bitcoin']]
+        end
+      end
+      get '/rack'
+      expect(last_response.status).to eq 402
+      expect(last_response.body).to eq 'Buy bitcoin'
+    end
+
     it 'ensures current behaviour when no route params are present does not change' do
       @app_class = Class.new(Apiculture::App) do
         extend Apiculture
-      
+
         param :number, "Number of the thing", Integer, cast: :to_i
         api_method :post, '/thing' do
           raise "Behaviour changed" unless params[:number] == 123
@@ -273,7 +298,7 @@ describe "Apiculture" do
     it 'applies a symbol typecast by calling a method on the route parameter value' do
       @app_class = Class.new(Apiculture::App) do
         extend Apiculture
-      
+
         route_param :number, "Number of the thing", Integer, :cast => :to_i
         api_method :post, '/thing/:number' do
           raise "Not cast" unless params[:number] == 123
@@ -288,7 +313,7 @@ describe "Apiculture" do
     it 'cast block arguments to the right type', run: true do
       @app_class = Class.new(Apiculture::App) do
         extend Apiculture
-      
+
         route_param :number, "Number of the thing", Integer, :cast => :to_i
         api_method :post, '/thing/:number' do |number|
           raise "Not cast" unless number.is_a?(Integer)
@@ -297,18 +322,18 @@ describe "Apiculture" do
       end
       post '/thing/123'
       expect(last_response.body).to eq('Total success')
-    
+
       # Double checking that bignums are okay, too
       bignum = 10**30
       post "/thing/#{bignum}"
       expect(last_response.body).to eq('Total success')
     end
 
-    
+
     it 'merges route_params and regular params' do
       @app_class = Class.new(Apiculture::App) do
         extend Apiculture
-      
+
         param :number, "Number of the thing", Integer, :cast => :to_i
         route_param :id, "Id of the thingy", Integer, :cast => :to_i
         route_param :awesome, "Hash of the thingy"
@@ -327,7 +352,7 @@ describe "Apiculture" do
     it 'applies a Proc typecast by calling the proc (for example - for ISO8601 time)' do
       @app_class = Class.new(Apiculture::App) do
         extend Apiculture
-    
+
         required_param :when, "When it happened", Time, cast: ->(v){ Time.parse(v) }
         api_method :post, '/occurrence' do
           raise "Not cast" unless params[:when].year == 2015
@@ -339,7 +364,7 @@ describe "Apiculture" do
       expect(last_response.body).to eq('Total success')
     end
   end
-  
+
   context 'Sinatra instance method extensions' do
     it 'adds support for json_response' do
       @app_class = Class.new(Apiculture::App) do
@@ -348,7 +373,7 @@ describe "Apiculture" do
           json_response({foo: 'bar'})
         end
       end
-      
+
       get '/some-json'
       expect(last_response).to be_ok
       expect(last_response['Content-Type']).to include('application/json')
@@ -363,7 +388,7 @@ describe "Apiculture" do
           json_response({foo: 'bar'}, status: 201)
         end
       end
-      
+
       post '/some-json'
       expect(last_response.status).to eq(201)
     end
@@ -384,13 +409,13 @@ describe "Apiculture" do
           raise "This should never be called"
         end
       end
-      
+
       get '/simple-halt'
       expect(last_response.status).to eq(400)
       expect(last_response['Content-Type']).to include('application/json')
       parsed_body = JSON.load(last_response.body)
       expect(parsed_body).to eq({"error"=>"Nein."})
-      
+
       get '/halt-with-error-payload'
       expect(last_response.status).to eq(400)
       expect(last_response['Content-Type']).to include('application/json')
