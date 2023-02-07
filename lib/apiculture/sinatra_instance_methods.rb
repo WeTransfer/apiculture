b4e29f394ae23ad2a47cfb8a44dd543f08853cf8
Ivan Danci <danci.ivan@gmail.com>
2023-02-07 12:44:12 +0100

    chore: add ruby 3.2 to the matrix

--- lib/apiculture/sinatra_instance_methods.rb
+++ lib/apiculture/sinatra_instance_methods.rb
@@ -4,7 +4,7 @@ require 'json'
 module Apiculture::SinatraInstanceMethods
   NEWLINE = "\n"
   DEFAULT = :__default__
-  
+
   # Convert the given structure to JSON, set the content-type and
   # return the JSON string
   def json_response(structure, status: DEFAULT)
@@ -12,7 +12,7 @@ module Apiculture::SinatraInstanceMethods
     status(status) unless status == DEFAULT
     JSON.pretty_generate(structure)
   end
-  
+
   # Bail out from an action by sending a halt() via Sinatra. Is most useful for
   # handling access denied, invalid resource and other types of situations
   # where you don't want the request to continue, but still would like to
@@ -20,10 +20,10 @@ module Apiculture::SinatraInstanceMethods
   # with it's own JSON means.
   def json_halt(with_error_message, status: 400, **attrs_for_json_response)
     # Pretty-print + newline to be terminal-friendly
-    err_str = JSON.pretty_generate({error: with_error_message}.merge(attrs_for_json_response)) + NEWLINE
+    err_str = JSON.pretty_generate({error: with_error_message}.merge(**attrs_for_json_response)) + NEWLINE
     halt status, {'Content-Type' => 'application/json'}, [err_str]
   end
-  
+
   # Handles the given action via the given class, passing it the instance variables
   # given in the keyword arguments
   def action_result(action_class, **action_ivars)
@@ -31,7 +31,7 @@ module Apiculture::SinatraInstanceMethods
     unless call_result.is_a?(Array) || call_result.is_a?(Hash) || (call_result.nil? && @status == 204)
       raise "Action result should be an Array, a Hash or it can be nil but only if status is 204, instead it was a #{call_result.class}"
     end
-  
+
     json_response call_result if call_result
   end
 end
